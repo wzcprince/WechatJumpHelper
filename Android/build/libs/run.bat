909cc330ee08d2d345137aeb8be7ef5710f4ee10
wzcprince <wzcprince@gmail.com>
2018-01-04 14:55:44 +0800

    增加run.bat

---
+++ Android/build/libs/run.bat
@@ -0,0 +1,13 @@
+:: cmd.exe
+
+chcp 936
+
+set PATH=%PATH%;D:\!learn\code\from_github\tools\android-platform-tools;
+
+echo %PATH%
+
+adb pull /sdcard/screen.png C:\Users\thinkpad\Desktop\temp\
+
+"C:\Program Files\Java\jdk-9.0.1\bin\java.exe" -jar Android.jar
+
+pause 
